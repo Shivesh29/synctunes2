dfb8f28b80b471d2ee0a97239842e6624995f8c6
Shivesh29 <sharmashivesh29@gmail.com>
2025-04-23 01:01:30 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Callback from "./pages/Callback";
+import NotFound from "./pages/NotFound";
+import PlaylistSelection from "./pages/PlaylistSelection";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/transfer" element={<Index />} />
+          <Route path="/playlists" element={<PlaylistSelection />} />
+          <Route path="/callback" element={<Callback />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
